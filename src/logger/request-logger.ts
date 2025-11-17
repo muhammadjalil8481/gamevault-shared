15489296c074fd29d8c7303dd55870fcfe217b82
muhammadjalil <muhammadjalil8481@gmail.com>
2025-11-17 19:54:25 +0500

    first commut

---
+++ src/logger/request-logger.ts
@@ -0,0 +1,33 @@
+import expressWinston from "express-winston";
+import { Logger } from "winston";
+
+const WinstonRequestLogger = (logger: Logger) => {
+  return expressWinston.logger({
+    winstonInstance: logger,
+    meta: true, // log headers, body, query
+    colorize: false,
+    ignoreRoute: (_req) => false, // can ignore certain routes if needed
+    level: function (_req, res) {
+      if (res.statusCode >= 400) return "error";
+      return "info";
+    },
+    format: logger.format,
+    msg: (req, res) => {
+      const body = Object.keys(req.body || {}).length
+        ? `\nbody: ${JSON.stringify(req.body)}`
+        : "";
+      const query = Object.keys(req.query || {}).length
+        ? `\nquery: ${JSON.stringify(req.query)}`
+        : "";
+      return `${req.method} ${res.statusCode} ${req.path}${query}${body}`;
+    },
+    dynamicMeta: (_req, _res) => {
+      return {
+        type: "operational",
+        source: "express-winston",
+      };
+    },
+  });
+};
+
+export default WinstonRequestLogger;
